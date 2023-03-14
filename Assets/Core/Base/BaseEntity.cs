0583c1286d361d06f2928a0028de193ba258d48e
XDenizX <denizik1@mail.ru>
2023-03-15 01:04:07 +0300

    Added base entity class.

---
+++ Assets/Core/Base/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Core.Interfaces;
+
+namespace Core.Base
+{
+    public class BaseEntity : IEntity
+    {
+        private float _lifetime = 20f;
+        
+        public event EventHandler Destroyed;
+        
+        public virtual void Destroy()
+        {
+            Destroyed?.Invoke(this, EventArgs.Empty);
+            EventBus.OnDestroy(this);
+        }
+        
+        public virtual void Simulate(float deltaTime)
+        {
+            _lifetime -= deltaTime;
+            
+            if (_lifetime < 0f)
+                Destroy();
+        }
+    }
+}
