cbb23999fec5e2299ebd56ae77b2e9e90c86fe30
XDenizX <denizik1@mail.ru>
2023-03-15 01:03:43 +0300

    Added event bus.

---
+++ Assets/Core/EventBus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Core.Interfaces;
+
+namespace Core
+{
+    public static class EventBus
+    {
+        public static event Action<IEntity> Created;
+        public static event Action<IEntity> Destroyed;
+        public static event Action<ICosmicBody> CosmicBodyHit;
+
+        public static void OnCreated(IEntity entity)
+        {
+            Created?.Invoke(entity);
+        }
+        
+        public static void OnDestroy(IEntity entity)
+        {
+            Destroyed?.Invoke(entity);
+        }
+        
+        public static void OnCosmicBodyHit(ICosmicBody cosmicBody)
+        {
+            CosmicBodyHit?.Invoke(cosmicBody);
+        }
+    }
+}
