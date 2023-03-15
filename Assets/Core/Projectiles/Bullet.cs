6f5fdc7fbdf6739a8ad56728d79389375b641561
XDenizX <denizik1@mail.ru>
2023-03-16 01:06:12 +0300

    Added bullet and ray projectiles.

--- Assets/Core/Projectiles/Bullet.cs
+++ Assets/Core/Projectiles/Bullet.cs
@@ -1,7 +1,57 @@
-﻿namespace Core.Projectiles
+﻿using System;
+using System.Numerics;
+using Core.Base;
+using Core.Utils;
+
+namespace Core.Projectiles
 {
-    public class Bullet
+    public class Bullet : BaseProjectile
     {
+        private float _radius;
+        private Vector2 _position;
+        private float _direction;
+
+        public event EventHandler<Vector2> PositionChanged;
+        public event EventHandler<float> RadiusChanged;
+        public event EventHandler<float> DirectionChanged;
+
+        public float Speed { get; set; }
+
+        public float Direction
+        {
+            get => _direction;
+            set
+            {
+                _direction = value;
+                DirectionChanged?.Invoke(this, _direction);
+            }
+        }
         
+        public Vector2 Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                PositionChanged?.Invoke(this, _position);
+            }
+        }
+
+        public float Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                RadiusChanged?.Invoke(this, _radius);
+            }
+        }
+
+        public override void Simulate(float deltaTime)
+        {
+            base.Simulate(deltaTime);
+            
+            Position += MathUtils.AngleToVector(Direction) * Speed * deltaTime;
+        }
     }
 }
--- Assets/Core/Projectiles/Ray.cs
+++ Assets/Core/Projectiles/Ray.cs
@@ -1,7 +1,92 @@
-﻿namespace Core.Projectiles
+﻿using System;
+using System.Numerics;
+using Core.Base;
+using Core.Interfaces;
+
+namespace Core.Projectiles
 {
-    public class Ray
+    public class Ray : BaseProjectile
     {
+        private const float RayWidth = 1f;
+        private const float MaxLength = 150f;
+        private const float PropagationSpeed = 30f;
         
+        private readonly ISpaceship _spaceship;
+
+        private Vector2 _emissionPoint;
+        private float _length;
+        private float _direction;
+        private float _width;
+
+        public event EventHandler<Vector2> EmissionPointChanged;
+        public event EventHandler<float> LengthChanged;
+        public event EventHandler<float> DirectionChanged;
+        public event EventHandler<float> LifetimeChanged;
+
+        public Vector2 EmissionPoint
+        {
+            get => _emissionPoint;
+            set
+            {
+                _emissionPoint = value;
+                EmissionPointChanged?.Invoke(this, _emissionPoint);
+            }
+        }
+        
+        public float Length
+        {
+            get => _length;
+            set
+            {
+                _length = value;
+                LengthChanged?.Invoke(this, _length);
+            }
+        }
+
+        public float Direction
+        {
+            get => _direction;
+            set
+            {
+                _direction = value;
+                DirectionChanged?.Invoke(this, _direction);
+            }
+        }
+        
+        public float Width
+        {
+            get => _width;
+            set
+            {
+                _width = value;
+                LifetimeChanged?.Invoke(this, _width);
+            }
+        }
+
+        public Ray(ISpaceship spaceship)
+        {
+            _spaceship = spaceship;
+            Width = RayWidth;
+        }
+        
+        public override void Simulate(float deltaTime)
+        {
+            base.Simulate(deltaTime);
+            
+            EmissionPoint = _spaceship.Position;
+            Direction = _spaceship.Angle;
+
+            if (_length < MaxLength)
+                Length += PropagationSpeed * deltaTime;
+
+            if (Width < 0)
+            {
+                Destroy();
+            }
+            else
+            {
+                Width -= deltaTime;
+            }
+        }
     }
 }
