baa04a744835a621475a56b968938be8178ffeaf
XDenizX <denizik1@mail.ru>
2023-03-16 01:05:52 +0300

    Added base projectile class.

---
+++ Assets/Core/Base/BaseProjectile.cs
@@ -0,0 +1,12 @@
+﻿using Core.Interfaces;
+
+namespace Core.Base
+{
+    public abstract class BaseProjectile : BaseEntity, IProjectile
+    {
+        public virtual void Hit(ICosmicBody cosmicBody)
+        {
+            cosmicBody.OnHit(this);
+        }
+    }
+}
