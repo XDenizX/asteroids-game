ed7bc53b64c95b5453f2b742d8990123a99fbc31
XDenizX <denizik1@mail.ru>
2023-03-16 01:07:03 +0300

    Added base weapon class.

---
+++ Assets/Core/Base/BaseWeapon.cs
@@ -0,0 +1,46 @@
+﻿using Core.Interfaces;
+
+namespace Core.Base
+{
+    public abstract class BaseWeapon<TProjectile> : IWeapon where TProjectile : IProjectile
+    {
+        protected abstract TProjectile CreateProjectile();
+
+        public ISpaceship Owner { get; set; }
+
+        public abstract bool CanShoot();
+        
+        public abstract void Simulate(float deltaTime);
+
+        protected BaseWeapon(ISpaceship owner)
+        {
+            Owner = owner;
+        }
+
+        public virtual void OnShoot()
+        {
+            // Empty implementation.
+        }
+
+        public bool TryShoot(out IProjectile projectile)
+        {
+            bool canShoot = CanShoot();
+            if (canShoot)
+            {
+                projectile = CreateProjectile();
+                OnShoot();
+            }
+            else
+            {
+                projectile = null;
+            }
+
+            return canShoot;
+        }
+
+        public void Destroy()
+        {
+            EventBus.OnDestroy(this);
+        }
+    }
+}
