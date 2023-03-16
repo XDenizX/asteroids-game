3b3d9c95c683a782b2ece27f0b31d2c6e5f3e9ae
XDenizX <denizik1@mail.ru>
2023-03-17 01:09:16 +0300

    Added base asteroid class.

---
+++ Assets/Core/Base/BaseCosmicBody.cs
@@ -0,0 +1,18 @@
+﻿using Core.Interfaces;
+
+namespace Core.Base
+{
+    public abstract class BaseCosmicBody : BaseEntity, ICosmicBody
+    {
+        public void Damage(ISpaceship spaceship)
+        {
+            spaceship.Destroy();
+        }
+
+        public virtual void OnHit(IProjectile projectile)
+        {
+            EventBus.OnCosmicBodyHit(this);
+            Destroy();
+        }
+    }
+}
