d0921ea49aea3df7ac66c490518d0688c13b26d7
XDenizX <denizik1@mail.ru>
2023-03-17 01:10:24 +0300

    Added base spawner class.

---
+++ Assets/Core/Base/BaseSpawner.cs
@@ -0,0 +1,31 @@
+﻿using Core.Interfaces;
+
+namespace Core.Base
+{
+    public abstract class BaseSpawner<T> : BaseEntity where T : IEntity
+    {
+        private float _produceTimeLeft;
+
+        private readonly float _produceTime;
+
+        public abstract T Produce();
+
+        protected BaseSpawner(float produceTime)
+        {
+            _produceTime = produceTime;
+        }
+        
+        public override void Simulate(float deltaTime)
+        {
+            _produceTimeLeft += deltaTime;
+
+            if (!(_produceTimeLeft > _produceTime))
+                return;
+            
+            T spawnedEntity = Produce();
+            EventBus.OnCreated(spawnedEntity);
+
+            _produceTimeLeft = 0;
+        }
+    }
+}
