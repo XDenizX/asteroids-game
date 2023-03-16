90aa8ec0238c2826f5b1fa3075788736fc65355d
XDenizX <denizik1@mail.ru>
2023-03-17 01:09:39 +0300

    Added asteroid and fragment cosmic bodies.

--- Assets/Core/CosmicBodies/Asteroid.cs
+++ Assets/Core/CosmicBodies/Asteroid.cs
@@ -1,7 +1,35 @@
-﻿namespace Core.CosmicBodies
+﻿using Core.Interfaces;
+using Core.Projectiles;
+using Core.Utils;
+
+namespace Core.CosmicBodies
 {
-    public class Asteroid
+    public class Asteroid : Fragment
     {
-        
+        private const int FragmentsCount = 3;
+
+        public override void OnHit(IProjectile projectile)
+        {
+            base.OnHit(projectile);
+            
+            if (projectile is not Bullet)
+                return;
+
+            CreateFragments();
+        }
+
+        private void CreateFragments()
+        {
+            for (var i = 0; i < FragmentsCount; i++)
+            {
+                var fragment = new Fragment
+                {
+                    Direction = MathUtils.RandomOnCircle(),
+                    Position = Position,
+                    Speed = Speed * 1.5f
+                };
+                EventBus.OnCreated(fragment);
+            }
+        }
     }
 }
--- Assets/Core/CosmicBodies/Fragment.cs
+++ Assets/Core/CosmicBodies/Fragment.cs
@@ -1,7 +1,44 @@
-﻿namespace Core.CosmicBodies
+﻿using System;
+using System.Numerics;
+using Core.Base;
+
+namespace Core.CosmicBodies
 {
-    public class Fragment
+    public class Fragment : BaseCosmicBody
     {
+        private Vector2 _position;
+        private Vector2 _direction;
+
+        public event EventHandler<Vector2> PositionChanged;
+        public event EventHandler<Vector2> DirectionChanged;
+
+        public float Speed { get; set; }
         
+        public Vector2 Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                PositionChanged?.Invoke(this, _position);
+            }
+        }
+        
+        public Vector2 Direction
+        {
+            get => _direction;
+            set
+            {
+                _direction = value;
+                DirectionChanged?.Invoke(this, _direction);
+            }
+        }
+        
+        public override void Simulate(float deltaTime)
+        {
+            base.Simulate(deltaTime);
+            
+            Position += _direction * Speed * deltaTime;
+        }
     }
 }
