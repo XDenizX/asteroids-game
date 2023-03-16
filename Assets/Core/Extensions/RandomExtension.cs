cbd5ca512389542d43478ddf1e1c3fa1e200b57b
XDenizX <denizik1@mail.ru>
2023-03-17 01:08:15 +0300

    Added random extension.

---
+++ Assets/Core/Extensions/RandomExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Extensions
+{
+    public static class RandomExtension
+    {
+        private static readonly Random Random = new();
+        
+        public static T GetRandom<T>(this IList<T> list)
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("The source sequence is empty.");
+
+            int randomIndex = Random.Next(0, list.Count);
+            
+            return list.ElementAt(randomIndex);
+        }
+    }
+}
