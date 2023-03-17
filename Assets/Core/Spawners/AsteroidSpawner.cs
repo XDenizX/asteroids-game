814e0cb29659f50e424e5db7123aa7a3db385587
XDenizX <denizik1@mail.ru>
2023-03-18 01:11:52 +0300

    Added asteroid and saucer spawners.

---
+++ Assets/Core/Spawners/AsteroidSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+using Core.Base;
+using Core.CosmicBodies;
+using Core.Utils;
+
+namespace Core.Spawners
+{
+    public class AsteroidSpawner : BaseSpawner<Asteroid>
+    {
+        private readonly Random _random = new();
+        
+        public float SpawnRadius { get; set; }
+        public float TargetAreaRadius { get; set; }
+        
+        public AsteroidSpawner(float produceTime) : base(produceTime)
+        { }
+
+        public override Asteroid Produce()
+        {
+            var speed = (float)(1 + _random.NextDouble());
+            Vector2 spawnPosition = MathUtils.RandomOnCircle(SpawnRadius);
+            Vector2 direction = GetRandomDirection(spawnPosition);
+
+            return new Asteroid
+            {
+                Position = spawnPosition,
+                Direction = direction,
+                Speed = speed
+            };
+        }
+
+        private Vector2 GetRandomDirection(Vector2 spawnPosition)
+        {
+            Vector2 targetPosition = MathUtils.RandomOnCircle(TargetAreaRadius);
+            Vector2 direction = targetPosition - spawnPosition;
+
+            return Vector2.Normalize(direction);
+        }
+    }
+}
---
+++ Assets/Core/Spawners/SaucerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using Core.Base;
+using Core.CosmicBodies;
+using Core.Interfaces;
+using Core.Utils;
+
+namespace Core.Spawners
+{
+    public class SaucerSpawner : BaseSpawner<Saucer>
+    {
+        private readonly ISpaceship _spaceship;
+        
+        public float SpawnRadius { get; set; } 
+        
+        public SaucerSpawner(ISpaceship target, float produceTime) : base(produceTime)
+        {
+            _spaceship = target;
+        }
+
+        public override Saucer Produce()
+        {
+            Vector2 spawnPosition = MathUtils.RandomOnCircle(SpawnRadius);
+            
+            return new Saucer(_spaceship)
+            {
+                Position = spawnPosition
+            };
+        }
+    }
+}
