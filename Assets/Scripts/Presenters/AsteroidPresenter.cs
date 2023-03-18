c9feb648d3df6d80b811d38500146b451eb7465c
XDenizX <denizik1@mail.ru>
2023-03-19 01:19:06 +0300

    Added an asteroid view.

---
+++ Assets/Scripts/Presenters/AsteroidPresenter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Base;
+using Core.CosmicBodies;
+using Core.Interfaces;
+using UnityEngine;
+using Views;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Presenters
+{
+    public class AsteroidPresenter : BasePresenter<Asteroid, AsteroidView>
+    {
+        public AsteroidPresenter(Asteroid model, AsteroidView view) : base(model, view)
+        { }
+
+        public override void Enable()
+        {
+            Model.PositionChanged += OnPositionChanged;
+            Model.DirectionChanged += OnDirectionChanged;
+            Model.Destroyed += OnDestroyed;
+        }
+
+        public override void Disable()
+        {
+            Model.PositionChanged -= OnPositionChanged;
+            Model.DirectionChanged -= OnDirectionChanged;
+            Model.Destroyed -= OnDestroyed;
+        }
+        
+        public void OnHit(IProjectile projectile)
+        {
+            Model.OnHit(projectile);
+        }
+        
+        private void OnDestroyed(object sender, EventArgs e)
+        {
+            Disable();
+        }
+
+        private void OnDirectionChanged(object sender, Vector2 direction)
+        {
+            var directionVector = new UnityEngine.Vector2(direction.X, direction.Y);
+            float angle = UnityEngine.Vector2.Angle(UnityEngine.Vector2.right, directionVector);
+            View.SetRotation(Quaternion.Euler(0, 0, angle));
+        }
+
+        private void OnPositionChanged(object sender, Vector2 position)
+        {
+            View.SetPosition(new UnityEngine.Vector2(position.X, position.Y));
+        }
+    }
+}
---
+++ Assets/Scripts/Views/AsteroidView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Base;
+using Components;
+using Core.Extensions;
+using Core.Interfaces;
+using Interfaces;
+using Presenters;
+using UnityEngine;
+
+namespace Views
+{
+    [RequireComponent(typeof(BlowEffect))]
+    public class AsteroidView : BaseView<AsteroidPresenter>, ICosmicBodyView
+    {
+        [SerializeField] private List<Sprite> asteroidsSprites;
+        [SerializeField] private SpriteRenderer body;
+
+        private BlowEffect _blowEffect;
+        
+        private void Start()
+        {
+            _blowEffect = GetComponent<BlowEffect>();
+            
+            body.sprite = asteroidsSprites.GetRandom();
+        }
+        
+        public void OnHit(IProjectile projectile)
+        {
+            Presenter.OnHit(projectile);
+            _blowEffect.Play();
+        }
+    }
+}
