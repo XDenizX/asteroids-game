cfb1362d51ee145a607486f1148808995c1888d3
XDenizX <denizik1@mail.ru>
2023-03-19 01:19:36 +0300

    Added a fragment view.

---
+++ Assets/Scripts/Presenters/FragmentPresenter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+using Base;
+using Core.CosmicBodies;
+using Core.Interfaces;
+using Views;
+using Quaternion = UnityEngine.Quaternion;
+
+namespace Presenters
+{
+    public class FragmentPresenter : BasePresenter<Fragment, FragmentView>
+    {
+        public FragmentPresenter(Fragment model, FragmentView view) : base(model, view)
+        { }
+        
+        public override void Enable()
+        {
+            Model.DirectionChanged += OnDirectionChanged;
+            Model.PositionChanged += OnPositionChanged;
+            Model.Destroyed += OnDestroyed;
+        }
+
+        public override void Disable()
+        {
+            Model.DirectionChanged -= OnDirectionChanged;
+            Model.PositionChanged -= OnPositionChanged;
+            Model.Destroyed -= OnDestroyed;
+        }
+        
+        public void OnHit(IProjectile projectile)
+        {
+            Model.OnHit(projectile);
+        }
+        
+        private void OnDestroyed(object sender, EventArgs e)
+        {
+            Disable();
+        }
+
+        private void OnPositionChanged(object sender, Vector2 position)
+        {
+            View.SetPosition(new UnityEngine.Vector2(position.X, position.Y));
+        }
+
+        private void OnDirectionChanged(object sender, Vector2 direction)
+        {
+            var directionVector = new UnityEngine.Vector2(direction.X, direction.Y);
+            float angle = UnityEngine.Vector2.Angle(UnityEngine.Vector2.right, directionVector);
+            View.SetRotation(Quaternion.Euler(0, 0, angle));
+        }
+    }
+}
---
+++ Assets/Scripts/Views/FragmentView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Base;
+using Core.Extensions;
+using Core.Interfaces;
+using Interfaces;
+using Presenters;
+using UnityEngine;
+
+namespace Views
+{
+    public class FragmentView : BaseView<FragmentPresenter>, ICosmicBodyView
+    {
+        [SerializeField] private List<Sprite> fragmentsSprites;
+        [SerializeField] private SpriteRenderer body;
+
+        private void Start()
+        {
+            body.sprite = fragmentsSprites.GetRandom();
+        }
+
+        public void OnHit(IProjectile projectile)
+        {
+            Presenter.OnHit(projectile);
+        }
+    }
+}
