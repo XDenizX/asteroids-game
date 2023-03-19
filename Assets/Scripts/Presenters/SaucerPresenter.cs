ff70d7fe7e40d9276f792a2401089ff3b414de80
XDenizX <denizik1@mail.ru>
2023-03-20 01:20:03 +0300

    Added a saucer view.

---
+++ Assets/Scripts/Presenters/SaucerPresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+using Base;
+using Core.CosmicBodies;
+using Core.Interfaces;
+using Views;
+
+namespace Presenters
+{
+    public class SaucerPresenter : BasePresenter<Saucer, SaucerView>
+    {
+        public SaucerPresenter(Saucer model, SaucerView view) : base(model, view)
+        { }
+
+        public override void Enable()
+        {
+            Model.PositionChanged += OnPositionChanged;
+            Model.Destroyed += OnDestroyed;
+        }
+
+        public override void Disable()
+        {
+            Model.PositionChanged -= OnPositionChanged;
+            Model.Destroyed -= OnDestroyed;
+        }
+        
+        public void OnHit(IProjectile projectile)
+        {
+            Model.OnHit(projectile);
+        }
+        
+        private void OnDestroyed(object sender, EventArgs e)
+        {
+            Disable();
+        }
+
+        private void OnPositionChanged(object sender, Vector2 position)
+        {
+            View.SetPosition(new UnityEngine.Vector2(position.X, position.Y));
+        }
+    }
+}
---
+++ Assets/Scripts/Views/SaucerView.cs
@@ -0,0 +1,26 @@
+﻿using Base;
+using Components;
+using Core.Interfaces;
+using Interfaces;
+using Presenters;
+using UnityEngine;
+
+namespace Views
+{
+    [RequireComponent(typeof(BlowEffect))]
+    public class SaucerView : BaseView<SaucerPresenter>, ICosmicBodyView
+    {
+        private BlowEffect _blowEffect;
+        
+        private void Start()
+        {
+            _blowEffect = GetComponent<BlowEffect>();
+        }
+
+        public void OnHit(IProjectile projectile)
+        {
+            Presenter.OnHit(projectile);
+            _blowEffect.Play();
+        }
+    }
+}
