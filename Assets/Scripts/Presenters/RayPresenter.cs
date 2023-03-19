d2c6f94ea2494849947aff94c1c14e676c6da60b
XDenizX <denizik1@mail.ru>
2023-03-20 01:20:36 +0300

    Added a ray view.

---
+++ Assets/Scripts/Presenters/RayPresenter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Numerics;
+using Base;
+using Core.Interfaces;
+using Core.Projectiles;
+using Views;
+using Quaternion = UnityEngine.Quaternion;
+
+namespace Presenters
+{
+    public class RayPresenter : BasePresenter<Ray, RayView>
+    {
+        public RayPresenter(Ray model, RayView view) : base(model, view)
+        { }
+
+        public override void Enable()
+        {
+            Model.EmissionPointChanged += OnEmissionPointChanged;
+            Model.DirectionChanged += OnDirectionChanged;
+            Model.LengthChanged += OnLengthChanged;
+            Model.LifetimeChanged += OnLifetimeChanged;
+            Model.Destroyed += OnDestroyed;
+        }
+
+        public override void Disable()
+        {
+            Model.EmissionPointChanged -= OnEmissionPointChanged;
+            Model.DirectionChanged -= OnDirectionChanged;
+            Model.LengthChanged -= OnLengthChanged;
+            Model.LifetimeChanged -= OnLifetimeChanged;
+            Model.Destroyed -= OnDestroyed;
+        }
+        
+        public IProjectile GetModel()
+        {
+            return Model;
+        }
+        
+        private void OnDestroyed(object sender, EventArgs e)
+        {
+            Disable();
+        }
+
+        private void OnLifetimeChanged(object sender, float lifetime)
+        {
+            View.SetWidth(lifetime);
+        }
+
+        private void OnLengthChanged(object sender, float length)
+        {
+            View.SetLength(length);
+        }
+
+        private void OnDirectionChanged(object sender, float direction)
+        {
+            View.SetRotation(Quaternion.Euler(0, 0, direction));
+        }
+
+        private void OnEmissionPointChanged(object sender, Vector2 emissionPoint)
+        {
+            View.SetPosition(new UnityEngine.Vector2(emissionPoint.X, emissionPoint.Y));
+        }
+    }
+}
---
+++ Assets/Scripts/Views/RayView.cs
@@ -0,0 +1,39 @@
+﻿using Base;
+using Components;
+using Interfaces;
+using Presenters;
+using UnityEngine;
+
+namespace Views
+{
+    public class RayView : BaseView<RayPresenter>
+    {
+        [SerializeField] private SpriteRenderer body;
+        [SerializeField] private AudioClip rayShotSound;
+
+        private void Start()
+        {
+            SoundManager.PlayClip(rayShotSound);
+        }
+
+        public void SetWidth(float width)
+        {
+            var bodyTransform = body.transform;
+            bodyTransform.localScale = new Vector3(width, bodyTransform.localScale.y);
+        }
+
+        public void SetLength(float length)
+        {
+            body.size = new Vector2(body.size.x, length);
+        }
+        
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            bool isCosmicBody = collision.TryGetComponent(out ICosmicBodyView cosmicBodyView);
+            if (!isCosmicBody)
+                return;
+
+            cosmicBodyView.OnHit(Presenter.GetModel());
+        }
+    }
+}
