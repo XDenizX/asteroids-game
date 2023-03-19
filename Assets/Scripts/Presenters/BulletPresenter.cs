4f90213901cc2b3d2980fe864b91ff384c85571b
XDenizX <denizik1@mail.ru>
2023-03-20 01:20:23 +0300

    Added a bullet view.

---
+++ Assets/Scripts/Presenters/BulletPresenter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Base;
+using Core.Projectiles;
+using UnityEngine;
+using Views;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Presenters
+{
+    public class BulletPresenter : BasePresenter<Bullet, BulletView>
+    {
+        public BulletPresenter(Bullet model, BulletView view) : base(model, view)
+        { }
+        
+        public override void Enable()
+        {
+            Model.PositionChanged += OnPositionChanged;
+            Model.RadiusChanged += OnRadiusChanged;
+            Model.DirectionChanged += OnDirectionChanged;
+            Model.Destroyed += OnDestroyed;
+            
+            View.SetRotation(Quaternion.Euler(0, 0, Model.Direction));
+        }
+
+        public override void Disable()
+        {
+            Model.PositionChanged -= OnPositionChanged;
+            Model.RadiusChanged -= OnRadiusChanged;
+            Model.DirectionChanged -= OnDirectionChanged;
+            Model.Destroyed -= OnDestroyed;
+        }
+        
+        public Bullet GetModel()
+        {
+            return Model;
+        }
+
+        public void Destroy()
+        {
+            Model.Destroy();
+        }
+
+        private void OnDestroyed(object sender, EventArgs e)
+        {
+            Disable();
+        }
+        
+        private void OnDirectionChanged(object sender, float direction)
+        {
+            View.SetRotation(Quaternion.Euler(0, 0, direction));
+        }
+
+        private void OnRadiusChanged(object sender, float radius)
+        {
+            View.SetRadius(radius);
+        }
+
+        private void OnPositionChanged(object sender, Vector2 position)
+        {
+            View.SetPosition(new UnityEngine.Vector2(position.X, position.Y));
+        }
+    }
+}
---
+++ Assets/Scripts/Views/BulletView.cs
@@ -0,0 +1,33 @@
+﻿using Base;
+using Components;
+using Interfaces;
+using Presenters;
+using UnityEngine;
+
+namespace Views
+{
+    public class BulletView : BaseView<BulletPresenter>
+    {
+        [SerializeField] private AudioClip bulletShotSound;
+
+        private void Start()
+        {
+            SoundManager.PlayClip(bulletShotSound);
+        }
+
+        public void SetRadius(float radius)
+        {
+            gameObject.transform.localScale = new Vector3(radius, radius, 1);
+        }
+        
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            bool isCosmicBody = collision.TryGetComponent(out ICosmicBodyView cosmicBodyView);
+            if (!isCosmicBody)
+                return;
+
+            cosmicBodyView.OnHit(Presenter.GetModel());
+            Presenter.Destroy();
+        }
+    }
+}
