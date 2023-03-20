b463ae77f427561ffad78fdcb7e6154900cb2c5b
XDenizX <denizik1@mail.ru>
2023-03-21 01:21:35 +0300

    Added a info panel view.

---
+++ Assets/Scripts/Presenters/InfoPanelPresenter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Base;
+using Core.Spaceships;
+using Core.Weapons;
+using Views;
+
+namespace Presenters
+{
+    public class InfoPanelPresenter : BasePresenter<Spaceship, InfoPanelView>
+    {
+        private Laser _laser;
+        
+        public InfoPanelPresenter(Spaceship model, InfoPanelView view) : base(model, view)
+        { }
+        
+        public override void Enable()
+        {
+            Model.PositionChanged += OnPositionChanged;
+            Model.AngleChanged += OnAngleChanged;
+            Model.SpeedChanged += OnSpeedChanged;
+            Model.Destroyed += OnDestroyed;
+
+            View.DisplayPosition(Model.Position);
+            View.DisplayAngle(Model.Angle);
+            View.DisplaySpeed(Model.Speed);
+            
+            _laser = Model.AvailableWeapons
+                .OfType<Laser>()
+                .FirstOrDefault();
+
+            if (_laser == null) 
+                return;
+            
+            _laser.AvailableAmmoChanged += OnAmmoChanged;
+            _laser.RollbackTimeLeftChanged += OnRollbackTimeChanged;
+            
+            View.DisplayLaserAmmo(_laser.AvailableAmmo);
+            View.DisplayLaserRollbackTime(_laser.RollbackTimeLeft);
+        }
+
+        public override void Disable()
+        {
+            Model.PositionChanged -= OnPositionChanged;
+            Model.AngleChanged -= OnAngleChanged;
+            Model.SpeedChanged -= OnSpeedChanged;
+            Model.Destroyed -= OnDestroyed;
+            
+            if (_laser == null)
+                return;
+            
+            _laser.AvailableAmmoChanged -= OnAmmoChanged;
+            _laser.RollbackTimeLeftChanged -= OnRollbackTimeChanged;
+        }
+
+        private void OnDestroyed(object sender, EventArgs e)
+        {
+            Disable();
+        }
+
+        private void OnRollbackTimeChanged(object sender, float time)
+        {
+            View.DisplayLaserRollbackTime(time);
+        }
+
+        private void OnAmmoChanged(object sender, int ammo)
+        {
+            View.DisplayLaserAmmo(ammo);
+        }
+
+        private void OnSpeedChanged(object sender, float speed)
+        {
+            View.DisplaySpeed(speed);
+        }
+
+        private void OnAngleChanged(object sender, float angle)
+        {
+            View.DisplayAngle(angle);
+        }
+
+        private void OnPositionChanged(object sender, Vector2 position)
+        {
+            View.DisplayPosition(position);
+        }
+    }
+}
---
+++ Assets/Scripts/Views/InfoPanelView.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using Base;
+using Presenters;
+using TMPro;
+using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Views
+{
+    public class InfoPanelView : BaseView<InfoPanelPresenter>
+    {
+        [SerializeField] private TextMeshProUGUI positionText;
+        [SerializeField] private TextMeshProUGUI angleText;
+        [SerializeField] private TextMeshProUGUI speedText;
+        [SerializeField] private TextMeshProUGUI laserAmmoText;
+        [SerializeField] private TextMeshProUGUI laserRollbackTimeText;
+
+        public void DisplayPosition(Vector2 position)
+        {
+            positionText.text = position.ToString("F2");
+        }
+        
+        public void DisplayAngle(float angle)
+        {
+            angleText.text = angle.ToString("0.00 °", CultureInfo.InvariantCulture);
+        }
+        
+        public void DisplaySpeed(float speed)
+        {
+            speedText.text = speed.ToString("F1", CultureInfo.InvariantCulture);
+        }
+        
+        public void DisplayLaserAmmo(int ammo)
+        {
+            laserAmmoText.text = ammo.ToString();
+        }
+        
+        public void DisplayLaserRollbackTime(float time)
+        {
+            laserRollbackTimeText.text = time.ToString("F1", CultureInfo.InvariantCulture);
+        }
+    }
+}
