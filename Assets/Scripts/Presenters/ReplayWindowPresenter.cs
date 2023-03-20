dc9df19612b254bbaba305bd672400a45db4a437
XDenizX <denizik1@mail.ru>
2023-03-21 01:21:22 +0300

    Added a replay window.

---
+++ Assets/Scripts/Presenters/ReplayWindowPresenter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Base;
+using Commands;
+using Core;
+using Views;
+
+namespace Presenters
+{
+    public class ReplayWindowPresenter : BasePresenter<Game, ReplayWindowView>
+    {
+        private readonly QuitCommand _quitCommand;
+        private readonly ReplayCommand _replayCommand;
+        
+        public ReplayWindowPresenter(Game model, ReplayWindowView view) : base(model, view)
+        {
+            _quitCommand = new QuitCommand();
+            _replayCommand = new ReplayCommand();
+        }
+
+        public override void Enable()
+        {
+            Model.Player.Destroyed += ShowReplayWindow;
+        }
+        
+        public override void Disable()
+        {
+            Model.Player.Destroyed -= ShowReplayWindow;
+        }
+
+        public void Replay()
+        {
+            _replayCommand.Execute(Model);
+        }
+
+        public void Quit()
+        {
+            _quitCommand.Execute(null);
+        }
+        
+        private void ShowReplayWindow(object sender, EventArgs e)
+        {
+            View.Show();
+            View.DisplayScore(Model.Score);
+        }
+    }
+}
---
+++ Assets/Scripts/Views/ReplayWindowView.cs
@@ -0,0 +1,46 @@
+﻿using Base;
+using Components;
+using Presenters;
+using TMPro;
+using UnityEngine;
+
+namespace Views
+{
+    public class ReplayWindowView : BaseView<ReplayWindowPresenter>
+    {
+        [SerializeField] private GameObject replayWindow;
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private AudioClip failSound;
+
+        private void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Show()
+        {
+            replayWindow.SetActive(true);
+            SoundManager.PlayClip(failSound);
+        }
+
+        public void Hide()
+        {
+            replayWindow.SetActive(false);
+        }
+        
+        public void DisplayScore(int score)
+        {
+            scoreText.text = $"Score: {score}";
+        }
+
+        public void Replay()
+        {
+            Presenter.Replay();
+        }
+
+        public void Quit()
+        {
+            Presenter.Quit();
+        }
+    }
+}
