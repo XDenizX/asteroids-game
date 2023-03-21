3d34b09e477c90eda953c755be58584c53bc1c26
XDenizX <denizik1@mail.ru>
2023-03-22 01:23:39 +0300

    Added a UI registrator.

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Core;
+using Interfaces;
+using Presenters;
+using UnityEngine;
+using Views;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] private InfoPanelView infoPanelView;
+    [SerializeField] private ReplayWindowView replayWindowView;
+
+    private List<IPresenter> _presenters = new();
+    
+    public void SetGame(Game game)
+    {
+        _presenters.ForEach(presenter => presenter.Disable());
+        
+        _presenters = new List<IPresenter>
+        {
+            new InfoPanelPresenter(game.Player, infoPanelView),
+            new ReplayWindowPresenter(game, replayWindowView)
+        };
+
+        _presenters.ForEach(presenter => presenter.Enable());
+    }
+}
